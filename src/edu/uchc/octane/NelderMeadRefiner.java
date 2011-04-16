f658c5b257ccc47c0d85f11e735ab3b48b6e6bde
Ji Yu <jiyu00@gmail.com>
2011-04-16 17:47:15 -0400

    Add a direct search optimizer

---
+++ src/edu/uchc/octane/NelderMeadRefiner.java
@@ -0,0 +1,172 @@
+//FILE:          NelderMeadRefiner.java
+//PROJECT:       Octane
+//-----------------------------------------------------------------------------
+//
+// AUTHOR:       Ji Yu, jyu@uchc.edu, 3/30/11
+//
+// LICENSE:      This file is distributed under the BSD license.
+//               License text is included with the source distribution.
+//
+//               This file is distributed in the hope that it will be useful,
+//               but WITHOUT ANY WARRANTY; without even the implied warranty
+//               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES./**
+//
+
+package edu.uchc.octane;
+
+import java.util.Arrays;
+
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.analysis.DifferentiableMultivariateVectorialFunction;
+import org.apache.commons.math.analysis.MultivariateMatrixFunction;
+import org.apache.commons.math.analysis.MultivariateRealFunction;
+import org.apache.commons.math.ConvergenceException;
+import org.apache.commons.math.optimization.GoalType;
+import org.apache.commons.math.optimization.RealPointValuePair;
+import org.apache.commons.math.optimization.VectorialPointValuePair;
+import org.apache.commons.math.optimization.direct.NelderMead;
+import org.apache.commons.math.optimization.general.GaussNewtonOptimizer;
+
+import ij.process.ImageProcessor;
+
+public class NelderMeadRefiner implements SubPixelRefiner, MultivariateRealFunction {
+	
+	static final int kernelSize_ = 2;
+	static final double defaultH_ = 200.0;
+	static final double sigma2_ = 1.73;
+	
+	//static double [] gaussLookupTable_;
+	
+//	static {
+//		gaussLookupTable_ = new double[(kernelSize_ * 2 + 1) * 100];
+//		//IJ.log("starting building index...");
+//		for (int i = 0 ; i < (kernelSize_ * 2 + 1) * 100; i ++ ) {
+//			gaussLookupTable_[i] = Math.exp(- (i * i) / (sigma2_ * 10000));
+//		}
+//		//IJ.log("Finishing building index...");
+//	}
+	
+	int blocks_;
+
+	int x0_,y0_;
+	ImageProcessor ip_;
+	double [] parameters_; 
+	double residue_;
+	double bg_ ;
+
+	public void setImageData(ImageProcessor ip){
+		ip_ = ip;
+		parameters_ = new double[4];
+		bg_ = ip.getAutoThreshold();		
+	}
+
+	double gauss(double x) {
+//		int x100 = (int) Math.round(Math.abs(x * 100)); 
+//		//return gaussLookupTable_[x100] * (x100 + 1 - x * 100) + gaussLookupTable_[x100+1] * (x * 100 - x100);
+//		return gaussLookupTable_[x100];
+		return Math.exp(- x*x/sigma2_);
+	}
+
+	@Override
+	public int refine(double x, double y) {
+		//int w = 1 + 2 * kernelSize_;
+		if (x < kernelSize_) {
+			x0_ = kernelSize_;
+		} else if (x >= ip_.getWidth() - kernelSize_) {
+			x0_ = ip_.getWidth() - kernelSize_ - 1;
+		} else { 
+			x0_ = (int) x;
+		}
+		parameters_[0] = x0_ + .5 - x;
+
+		if (y < kernelSize_) {
+			y0_ = kernelSize_;
+		} else if (y >= ip_.getHeight() - kernelSize_) {
+			y0_ = ip_.getHeight() - kernelSize_ - 1;
+		} else { 
+			y0_ = (int) y;
+		}
+		parameters_[1] = y0_ + .5 - y;
+
+		parameters_[3] = bg_;
+
+		try {
+			fit();
+		} catch (ConvergenceException e) {
+			return -1;
+		} catch (Exception e) {
+//			System.out.println(e.getMessage() + e.toString());
+			return -2;
+		}
+
+		// report
+//		double hw = 0.5 + kernelSize_;
+//		for (int xi = - kernelSize_; xi <= kernelSize_; xi++) {
+//			for (int yi = - kernelSize_; yi <= kernelSize_; yi++) {
+//				double xp = Math.sin(parameters_[0]) * hw;
+//				double yp = Math.sin(parameters_[1]) * hw;
+//				System.out.printf("%3d(%5f)\t", ip_.get(x0_ + xi, y0_ + yi), parameters_[2] * gauss(xp + xi) * gauss( yp + yi) + parameters_[3]);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+		
+		return 0;
+	}
+	
+	void fit() throws ConvergenceException, FunctionEvaluationException, IllegalArgumentException {
+		NelderMead nm = new NelderMead();
+		parameters_[2] = ip_.get(x0_, y0_) - bg_;
+
+		RealPointValuePair vp = nm.optimize(this, GoalType.MINIMIZE, parameters_);
+		parameters_ = vp.getPoint();
+		residue_ = vp.getValue() / parameters_[2] / parameters_[2]; // normalized to H^2
+	}
+
+	@Override
+	public double getXOut() {
+		return (x0_ + .5 - parameters_[0]);
+	}
+
+	@Override
+	public double getYOut() {
+		return (y0_ + .5 - parameters_[1]);
+	}
+
+	@Override
+	public double getHeightOut() {
+		return parameters_[2];
+	}
+
+	@Override
+	public double getResidue() {
+		return residue_ ;
+	}
+
+	@Override
+	public double value(double[] p) throws FunctionEvaluationException,IllegalArgumentException {
+		//double hw = 0.5 + kernelSize_;
+		double xp = p[0];
+		double yp = p[1];
+		double h = p[2];
+		double bg = p[3];
+		double r = 0;
+		if (bg < 0 || h < 0) {
+			return 1e10;
+		}
+		
+		for (int xi = - kernelSize_; xi <= kernelSize_; xi++) {
+			for (int yi = - kernelSize_; yi <= kernelSize_; yi++) {
+				double g = gauss(xp + xi)* gauss(yp + yi);
+				double v = (double)(ip_.get(x0_ + xi , y0_ + yi)) - bg - h*g;
+				r += v * v;
+			}
+		}
+
+		return r;
+	}
+}
