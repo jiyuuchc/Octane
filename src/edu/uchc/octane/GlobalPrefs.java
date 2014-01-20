6d954c38734e0868f390f598d573b0920527e3dd
Ji Yu <jyu@uchc.edu>
2014-01-20 11:04:12 -0500

    Add a dialog to change global preferences

--- src/edu/uchc/octane/GlobalPrefs.java
+++ src/edu/uchc/octane/GlobalPrefs.java
@@ -18,6 +18,7 @@
 package edu.uchc.octane;
 
 import ij.Prefs;
+import ij.gui.GenericDialog;
 
 import java.util.prefs.Preferences;
 
@@ -35,12 +36,16 @@ public class GlobalPrefs {
 	final private static String PARTICLE_ANALYSIS_MODE_KEY = "ParticleAnalysisMode";
 	final private static String ASTIGMATISM_CALIBRATION_X_KEY = "astigmatismCalibrationX";
 	final private static String ASTIGMATISM_CALIBRATION_Y_KEY = "astigmatismCalibrationY";
-
+	final private static String ALTERNATING_OVERLAY_COLOR = "AlternatingOverlayColor";
+	final private static String OVERLAY_STROKE_WIDTH = "OverlayStrokeWidth";
+	
 	public static int nThread_ = prefs_.getInt(NUM_THREAD_KEY , 4);
 	public static double defaultPixelSize_ = Prefs.getDouble(DEFAULT_PIXEL_SIZE_KEY, 160);
 	public static String particleAnalysisMode_ = Prefs.get(PARTICLE_ANALYSIS_MODE_KEY, "Accurate");
 	public static String calibrationStrX_ = GlobalPrefs.getRoot().get(ASTIGMATISM_CALIBRATION_X_KEY, "0.8, 0, 0.18");
 	public static String calibrationStrY_ = GlobalPrefs.getRoot().get(ASTIGMATISM_CALIBRATION_Y_KEY, "0.8, 6, 0.18");
+	public static boolean alternatingOverlayColor_ = GlobalPrefs.getRoot().getBoolean(ALTERNATING_OVERLAY_COLOR, true);
+	public static double overlayStrokeWidth_ = GlobalPrefs.getRoot().getDouble(OVERLAY_STROKE_WIDTH, 0.3);
 	
 	public static Preferences getRoot() {
 		return prefs_;
@@ -57,5 +62,34 @@ public class GlobalPrefs {
 		prefs_.put(ASTIGMATISM_CALIBRATION_X_KEY, calibrationStrX_);
 		prefs_.put(ASTIGMATISM_CALIBRATION_Y_KEY, calibrationStrY_);
 	
+	}
+	
+	/**
+	 * Open global preference dialog
+	 * 
+	 *  @return  True if user pressed OK
+	 */
+	
+	static public boolean openDialog(TrajDataset data, boolean b) {
+		
+		GenericDialog dlg = new GenericDialog("Octane Preferences"); 
+		
+		dlg.addNumericField("Numer of threads", nThread_, 0);
+		dlg.addNumericField("Default Pixel Size", defaultPixelSize_, 2);
+		
+		dlg.addMessage("Overlay Options");
+		dlg.addCheckbox("Alternating Trajectory Color", alternatingOverlayColor_);
+		dlg.addNumericField("Overlay Stroke Width", overlayStrokeWidth_, 2);
+
+		dlg.showDialog();
+		
+		if (dlg.wasCanceled()) {
+
+			return false;
+		}
+		
+		
+		
+		return true;
 	}
 }
